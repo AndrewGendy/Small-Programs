8cbc48a085191e024be8ba01727220e093efd509
AndrewGendy <mando201025@yahoo.com>
2019-10-17 12:40:47 -0400

    Initial commit

---
+++ Jagged Array.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace test {
+    class Program {
+        static void Main(string[] args) {
+
+            string[] students = new string[3];
+            students[0] = "Andrew";
+            students[1] = "Ahmet";
+            students[2] = "Ali";
+
+            string[][] jaggedArray = new string[3][];
+            jaggedArray[0] = new string[3];
+            jaggedArray[1] = new string[1];
+            jaggedArray[2] = new string[2];
+
+            jaggedArray[0][0] = "Bachelor";
+            jaggedArray[0][1] = "Masters";
+            jaggedArray[0][2] = "Doctorate";
+
+            jaggedArray[1][0] = "Bachelor";
+
+            jaggedArray[2][0] = "Bachelor";
+            jaggedArray[2][1] = "Masters";
+
+            for (int i = 0; i < jaggedArray.Length; i++) {
+                Console.WriteLine(students[i] + "\n-------");
+                for (int j = 0; j < jaggedArray[i].Length; j++) {
+                    Console.WriteLine(jaggedArray[i][j]);
+                }
+                Console.WriteLine();
+            }
+
+
+        }
+    }
+}
---
+++ Multiple Types Array.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace test {
+    class Program {
+        static void Main(string[] args) {
+
+            Customers Ali = new Customers {
+                FirstName = "Ali",
+                LastName = "Muhamad"
+            };
+
+            object[] array = new object[4];
+
+            array[0] = 1;
+            array[1] = 2.5;
+            array[2] = "Andrew";
+            array[3] = Ali;
+
+            foreach (object obj in array) {
+
+                Console.WriteLine(obj);
+            }
+        }
+
+        public class Customers {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+
+            public string GetFullName() {
+                return this.FirstName + " " + this.LastName;
+            }
+
+            public override string ToString() {
+                return this.GetFullName();
+            }
+
+        }
+    }
+
+
+}
